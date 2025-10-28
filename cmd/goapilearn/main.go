e52d20c80a1c534815848c671bba48204b19b2cb
BestKung <best.kittiporn@gmail.com>
2025-10-28 17:56:33 +0700

    first commit

---
+++ cmd/goapilearn/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	fmt.Printf("%T", app)
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Listen(":8080")
+}
